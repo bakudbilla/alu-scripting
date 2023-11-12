4de59228d78509887e519d75ebef47e903fe4b28
Bernice Akudbilla <b.akudbilla@alustudent.com>
2023-11-12 17:41:12 +0000

    task 2

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -4,7 +4,7 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):    
+def recurse(subreddit, hot_list=[], after=""):   
     URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
